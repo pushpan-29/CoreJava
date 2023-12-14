aff904801ff882978d6106229ba6efadf765b00d
pushpan-29 <pushpanarake@gmail.com>
2023-12-14 14:42:10 +0530

    [STNO-999]added Arithmatic Application

---
+++ CoreJava/src/com/cs/core/ArithmaticApp.java
@@ -0,0 +1,27 @@
+package com.cs.core;
+
+import java.util.Scanner;
+
+public class ArithmaticApp {
+
+        public static void main(String[] args) {
+
+           /*    String  empCode="1199";
+                double employeeCode=Float.valueOf(empCode);*/
+
+               /* String  empCode="1199";
+                double employeeCode=Long.valueOf(empCode);*/
+
+               /* String  empCode="1199";
+                double employeeCode=Long.valueOf(empCode);*/
+
+                String  empCode="1199";
+               int  employeeCode=Integer.valueOf(empCode);
+
+                System.out.println(employeeCode);
+        }
+
+        }
+
+
+
